9d256c487c0649ad8bfa1604d9c9d19a4ff65861
krisna <krisnasirait48@gmail.com>
2023-02-15 22:35:57 +0700

    Initial commit

---
+++ app/src/main/java/com/krisna/airwise/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krisna.airwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
