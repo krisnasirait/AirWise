16f08d946e1fa6c988a3b5c3c1613b654d96d319
krisna <krisnasirait48@gmail.com>
2023-02-19 21:35:42 +0700

    Implement AirVisual API with HTTP interceptor and view binding Update repository to use API key from build configuration

---
+++ app/src/main/java/com/krisna/airwise/data/AirVisualInterceptor.kt
@@ -0,0 +1,13 @@
+package com.krisna.airwise.data
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AirVisualInterceptor(private val apiKey: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("apikey", apiKey)
+            .build()
+        return chain.proceed(request)
+    }
+}
--- app/src/main/java/com/krisna/airwise/data/AirVisualRepository.kt
+++ app/src/main/java/com/krisna/airwise/data/AirVisualRepository.kt
@@ -2,6 +2,10 @@ package com.krisna.airwise.data
 
 import com.krisna.airwise.BuildConfig
 import com.krisna.airwise.data.models.AirVisualData
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class AirVisualRepository(private val service: AirVisualService) {
     suspend fun getNearestCity(): AirVisualData {
@@ -10,3 +14,16 @@ class AirVisualRepository(private val service: AirVisualService) {
         return response.data
     }
 }
+
+val okHttpClient = OkHttpClient.Builder()
+    .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+    .build()
+
+val retrofit = Retrofit.Builder()
+    .baseUrl("https://api.airvisual.com/v2/")
+    .client(okHttpClient)
+    .addConverterFactory(MoshiConverterFactory.create())
+    .build()
+
+val service = retrofit.create(AirVisualService::class.java)
+
